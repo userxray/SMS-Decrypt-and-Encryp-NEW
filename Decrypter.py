ee8cf6578f89da78c9446fbd99966c5a39c30b14
TurkishDARK <91960201+userxray@users.noreply.github.com>
2022-10-01 20:52:42 +0200

    Add files via upload

---
+++ Decrypter.py
@@ -0,0 +1,71 @@
+#Encrypter Verschluessler kilitle
+
+print("")
+
+print("Welcome to E N C R Y P T E R")
+print("coded by Mr. I. ESIA")
+print("")
+print("")
+print("Enter you'r TEXT you wan't to encrypt")
+print("(!possible for English keyboard and use only upper-case!)")
+print("")
+
+
+ausgabetext = name = input()
+
+
+ausgabetext = ausgabetext.replace("A", "1")
+
+ausgabetext = ausgabetext.replace("B", "2")
+
+ausgabetext = ausgabetext.replace("C", "3")
+
+ausgabetext = ausgabetext.replace("D", "4")
+
+ausgabetext = ausgabetext.replace("E", "5")
+
+ausgabetext = ausgabetext.replace("F", "6")
+
+ausgabetext = ausgabetext.replace("G", "7")
+
+ausgabetext = ausgabetext.replace("H", "8")
+
+ausgabetext = ausgabetext.replace("I", "9")
+
+ausgabetext = ausgabetext.replace("J", "!")
+
+ausgabetext = ausgabetext.replace("K", "§")
+
+ausgabetext = ausgabetext.replace("L", "%")
+
+ausgabetext = ausgabetext.replace("M", "&")
+
+ausgabetext = ausgabetext.replace("N", "/")
+
+ausgabetext = ausgabetext.replace("O", "(")
+
+ausgabetext = ausgabetext.replace("P", ")")
+
+ausgabetext = ausgabetext.replace("Q", "=")
+
+ausgabetext = ausgabetext.replace("R", "?")
+
+ausgabetext = ausgabetext.replace("S", "+")
+
+ausgabetext = ausgabetext.replace("T", "-")
+
+ausgabetext = ausgabetext.replace("U", ":")
+
+ausgabetext = ausgabetext.replace("V", ".")
+
+ausgabetext = ausgabetext.replace("W", ",")
+
+ausgabetext = ausgabetext.replace("X", "#")
+
+ausgabetext = ausgabetext.replace("Y", "~")
+
+ausgabetext = ausgabetext.replace("Z", "*")
+
+ausgabetext = ausgabetext.replace(" ", ">")
+
+print(ausgabetext)
---
+++ Encrypter.py
@@ -0,0 +1,71 @@
+#Decrypter entschlüssler kiliti ac
+
+print("")
+
+print("Welcome to D E C R Y P T E R")
+print("coded by Mr. I. ESIA")
+print("")
+print("")
+print("Enter you'r numbers and the symbols you wan't to decrypt")
+print("(!possible for English keyboard and use only upper-case!)")
+print("")
+
+
+ausgabetext = name = input()
+
+
+ausgabetext = ausgabetext.replace("1", "A")
+
+ausgabetext = ausgabetext.replace("2", "B")
+
+ausgabetext = ausgabetext.replace("3", "C")
+
+ausgabetext = ausgabetext.replace("4", "D")
+
+ausgabetext = ausgabetext.replace("5", "E")
+
+ausgabetext = ausgabetext.replace("6", "F")
+
+ausgabetext = ausgabetext.replace("7", "G")
+
+ausgabetext = ausgabetext.replace("8", "H")
+
+ausgabetext = ausgabetext.replace("9", "I")
+
+ausgabetext = ausgabetext.replace("!", "J")
+
+ausgabetext = ausgabetext.replace("§", "K")
+
+ausgabetext = ausgabetext.replace("$", "L")
+
+ausgabetext = ausgabetext.replace("&", "M")
+
+ausgabetext = ausgabetext.replace("/", "N")
+
+ausgabetext = ausgabetext.replace("(", "O")
+
+ausgabetext = ausgabetext.replace(")", "P")
+
+ausgabetext = ausgabetext.replace("=", "Q")
+
+ausgabetext = ausgabetext.replace("?", "R")
+
+ausgabetext = ausgabetext.replace("+", "S")
+
+ausgabetext = ausgabetext.replace("-", "T")
+
+ausgabetext = ausgabetext.replace(":", "U")
+
+ausgabetext = ausgabetext.replace(".", "V")
+
+ausgabetext = ausgabetext.replace(",", "W")
+
+ausgabetext = ausgabetext.replace("#", "X")
+
+ausgabetext = ausgabetext.replace("~", "Y")
+
+ausgabetext = ausgabetext.replace("*", "Z")
+
+ausgabetext = ausgabetext.replace(">", " ")
+
+print(ausgabetext)
